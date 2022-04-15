fd106ff7a3f9fe743228c1f776faabfd6f679dea
Jane <evgenya.mun@yandex.ru>
2022-04-15 03:13:09 +0300

    add changes 5

--- задача5/Program.cs
+++ задача5/Program.cs
@@ -12,7 +12,7 @@ Console.Write("Введите число: ");
 int b = Convert.ToInt32(Console.ReadLine());//конечное значение
 int a = -b; //начальное значение
 
-while (a <= b);
+while (a <= b)
 {
     Console.Write(a);
     a++;
